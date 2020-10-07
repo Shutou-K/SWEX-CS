120563dab1b0cb3008bec2ae18f6a18195bfa21e
EC2 Default User <ec2-user@ip-172-31-84-251.ec2.internal>
2020-10-07 07:02:38 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'top/main'
+  post 'top/login'
+  root 'top#main'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get "application/CS"
+  #root "application#CS"
+end
