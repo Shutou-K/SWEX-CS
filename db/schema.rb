2e588252923c6fc3b460dc76a4e2444e4f9d85ff
EC2 Default User <ec2-user@ip-172-31-84-251.ec2.internal>
2020-10-21 03:28:19 +0000

    heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_015530) do
+ActiveRecord::Schema.define(version: 2020_10_20_235101) do
 
   create_table "users", force: :cascade do |t|
     t.string "uid"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_10_21_015530) do
     t.string "pass_confirm"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
 end
